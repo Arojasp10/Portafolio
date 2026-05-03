8b4bd95ae63fe8c86597f53aea915a43684052e5
Arojasp10 <alexasagoru@gmail.com>
2026-05-02 22:21:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.markdown(
     .titulo {
         font-size: 48px;
         font-weight: bold;
-        color: #4CAF50;
+        color: #620462;
         text-align: center;
     }
     .subtitulo {
@@ -37,9 +37,27 @@ st.write("---")
 
 # Lista de proyectos
 proyectos = {
-    "📊 Proyecto 1 - ": "",
-    "📈 Proyecto 2 - ": "2",
-    "🤖 Proyecto 3 - ": "",
+    "🌟 Proyecto 1 - Clase 01": "https://intro-26-02-2026-mirprimerappeninterfaces10.streamlit.app/",
+    "✨ Proyecto 2 - Control por voz inteligente": "https://ctrlvoice-controlxvoz-arp07.streamlit.app/",
+    "🌟 Proyecto 3 - Tablero Inteligente": "https://drawrecode0111.streamlit.app/",
+    "✨ Proyecto 4 - Reconocimiento de digitos a mano": "https://gvg6qqomh6vofairthbflj.streamlit.app/",
+    "🌟 Proyecto 5 - Conversión texto-audio": "https://im-class6-labasequepasoelprofe.streamlit.app/",   
+    "✨ Proyecto 6 - RAG ": "https://chatpdf-563hjncbbazkw2zpmjkwq8.streamlit.app/",
+    "🌟 Proyecto 7 - Generador de Texto LSTM": "https://lstmnlp-kno59sekzgxzj4ss73dsec.streamlit.app/",
+    "✨ Proyecto 8 - Lector de imágenes": "https://7j5f9mvnfz4wv4ybjrk6zb.streamlit.app/",
+    "🌟 Proyecto 9 - Reconomiento óptico y transcripción": "https://ocr-audio-c7khzhtv5d3xhgarj3ww4b.streamlit.app/",
+    "✨ Proyecto 10 - Lector sensor MQTT": "https://recepmqtt-ang10.streamlit.app/",
+    "🌟 Proyecto 11 - MQTT Control": "https://sendcmqtt-ang10recibirmsj-mqtt.streamlit.app/",
+    "✨ Proyecto 12 - Análisis sentimiento": "https://sentimenta-8cpozjsvsqjmb8td7c6dce.streamlit.app/",
+    "🌟 Proyecto 13 - Tablero para dibujo": "https://tablero-cs5y4l5a4mxt7a37mbb5en.streamlit.app/",
+    "✨ Proyecto 14 - Demo TF-IDF Español": "https://tdfesp-bwvzb43a7mjkpdmmgzg58f.streamlit.app/",
+    "🌟 Proyecto 15 - Demo TF-IDF Preguntas y respuestas": "https://ddkivd7kpctzjvszaqjemt.streamlit.app/",
+    "✨ Proyecto 16 - Reconocimiento de Imágenes": "https://3ztdbjn8rti39uuup5wzvg.streamlit.app/",
+    "🌟 Proyecto 17 - Traductor": "https://traductor-dj7eknpxterzom6jcqksvx.streamlit.app/",
+    "✨ Proyecto 18 - Análisis de Imagen": "https://visionapp-fzhbfxynukfkteugdisjsd.streamlit.app/",
+    "🌟 Proyecto 19 - WorldCloud": "https://wordcloud-zv6qtjhrx4r5nw6uxev4xv.streamlit.app/",
+    "✨ Proyecto 20 - YOLO": "https://yolov5-pdstfhzmscsgf9ghwethfa.streamlit.app/",
+
 }
 
 # Mostrar proyectos en columnas tipo tarjetas
@@ -50,11 +68,11 @@ for i, (nombre, link) in enumerate(proyectos.items()):
             f"""
             <div class="card">
                 <h3>{nombre}</h3>
-                <a href="{link}" target="_blank">🔗 Ver en GitHub</a>
+                <a href="{link}" target="_blank">🔗 Ver</a>
             </div>
             """,
             unsafe_allow_html=True
         )
 
 st.write("---")
-st.success("✨ Portafolio creado con Streamlit. ¡Listo para desplegar en la nube!")
+
